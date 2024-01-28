5bc67d2f62dfea5a73206a29092cdce1ac0f610a
Leina Gray <abad.alleina@gmail.com>
2024-01-28 17:54:07 +0800

    ADD PLAYER CONSTANTS: add default positions and player image constants

--- settings.py
+++ settings.py
@@ -1,6 +1,12 @@
 # GAME DEFAULT SETTINGS SETUP
 
-# WINDOW SETUP
+# DEFAULT WINDOW SETUP
 WIDTH = 1280
 HEIGHT = 720
-BACKGROUND_FILE_PATH = "content/background/temp_background.png"
+BACKGROUND_IMG = "content/background/temp_background.png"
+FPS = 60
+
+# DEFAULT PLAYER SETUP
+PLAYER_DEFAULT_X = 0
+PLAYER_DEFAULT_Y = HEIGHT/2 #place to center
+PLAYER_IMG = "content/sprites/player.png"

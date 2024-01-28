e2a69a2eeaed500fc6565164f56d34b960ba079d
Leina Gray <abad.alleina@gmail.com>
2024-01-28 16:45:11 +0800

    SET WINDOW: use the window class to create a window, set the title, and set the background

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 import pygame 
+from settings import *
+from Window import *
+
+window = Window(WIDTH, HEIGHT, "Typocalypse")
+window.setBackground(BACKGROUND_FILE_PATH)
+
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            
+
+    # Update game elements and draw on the window here
+
+    pygame.display.update()  # Update the display

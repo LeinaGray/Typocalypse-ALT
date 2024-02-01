a80e54569f880f01869c88ed8632a73ccd170d11
Leina Gray <abad.alleina@gmail.com>
2024-02-01 19:51:34 +0800

    PLAYER SETUP: move player away from the wall

--- settings.py
+++ settings.py
@@ -4,10 +4,10 @@
 WIDTH = 1280
 HEIGHT = 720
 TILE_IMG = "content/background/temp_background.png"
-FPS = 60
+FPS = 1000
 
 # DEFAULT PLAYER SETUP
-PLAYER_DEFAULT_X = 0
+PLAYER_DEFAULT_X = 45 #further away from the wall
 PLAYER_DEFAULT_Y = HEIGHT/2 #place to center
 PLAYER_IMG = "content/sprites/player.png"
 SIZE = 0.35
